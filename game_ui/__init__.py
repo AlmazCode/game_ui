56486cab0b0a0fc1d10d6f5a1d60b739583f51a0
AlmazCode <104347420+AlmazCode@users.noreply.github.com>
2022-11-29 11:22:06 +0600

    Delete game_ui directory

--- game_ui/__init__.py
+++
@@ -1,7 +0,0 @@
-from .buttons import Button, Buttons
-from .input import Input, inputs
-from .text import Text, Texts
-
-__author__ = 'AlmazCode'
-__version__ = '1.6'
-__email__ = 'diamondplay43@gmail.com'
